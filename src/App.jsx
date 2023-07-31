c4b8beb2b8eae49390c9198f943fc1ee53864669
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-07-31 12:36:33 +0300

    set up

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { grapesjs } from "grapesjs"
+import { useState,useEffect } from "react"
+
+function App() {
+  const [editor,setEditor]=useState(null);
+
+  useEffect(()=>{
+    const editor=grapesjs.init({
+      container:"#editor"
+    });
+  },[])
+
+  return (
+    <>
+    <div className="App">
+      <div id="editor"></div>
+    </div>       
+    </>
+  )
+}
+
+export default App
+
