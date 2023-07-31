c079a77d9a6b7262f66af1dee47f32d69b1c8d4e
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-07-31 12:48:23 +0300

    set up grapesjs-preset-webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { grapesjs } from "grapesjs"
 import { useState,useEffect } from "react"
+import Plugin from "grapesjs-preset-webpage";
 
 function App() {
   const [editor,setEditor]=useState(null);
 
   useEffect(()=>{
     const editor=grapesjs.init({
-      container:"#editor"
+      container:"#editor",
+      plugins:[Plugin],
+      pluginsOpts:{
+        [Plugin]:{}
+      },
+      
     });
     setEditor(editor);
   },[])
