36c576f5a0b3f8e7127e98621401a03422b1d796
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-07-31 16:00:19 +0300

    set up mjml

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,32 @@ import "./styles/main.scss";
 import WebPlugin from "grapesjs-preset-webpage";
 import FormPlugin from "grapesjs-plugin-forms";
 import plugin from "grapesjs-blocks-basic";
+import MjmlPlugin from "grapesjs-mjml";
+import { Editor } from "./Editor";
 
 function App() {
   const [editor,setEditor]=useState(null);
 
-  useEffect(()=>{
-    const editor=grapesjs.init({
-      container:"#editor",
-      plugins:[plugin,WebPlugin,FormPlugin],
-      pluginsOpts:{
-        WebPlugin:{},
-        FormPlugin:{},
-        plugin:{flexGrid: true },
-      },
+  // useEffect(()=>{
+  //   const editor=grapesjs.init({
+  //     container:"#editor",
+  //     plugins:[MjmlPlugin,plugin,WebPlugin,FormPlugin,],
+  //     pluginsOpts:{
+  //       WebPlugin:{},
+  //       FormPlugin:{},
+  //       plugin:{flexGrid: true },
+  //       MjmlPlugin:{}
+  //     },
 
-    });
-    setEditor(editor);
-  },[])
+  //   });
+  //   setEditor(editor);
+  // },[])
 
   return (
     <>
     <div className="App">
       <div id="editor"></div>
+      <Editor/>
     </div>       
     </>
   )
